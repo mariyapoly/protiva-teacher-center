e422f77c2a53cc29659c46a009111092adfb35b2
mariyapoly <poly.craft2141@gmail.com>
2021-09-26 02:05:26 +0600

    created react app and added Header componen

---
+++ src/components/Header/Header.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container">
+                    <a className="navbar-brand" href="/">Protiva <span>school center</span> </a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/home">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/about">about</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/className">Classes
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/teachers">teaches</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/blog">blog</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/blog">contact</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div className="total-team">
+                <div className="container">
+                    <h3>Make an Avengers team</h3>
+                    <p>The city is flying and we are fighting an army of robots, and I have a bow and arrow</p>
+                    <h3>Total Budget: <span>100 Million</span> </h3>
+                </div>
+                <h2 className="page-title">Protiva</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/TeachersContainer/TeachersContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './TeachersContainer.css'
+
+const TeachersContainer = () => {
+    return (
+        <div className="teachers-container">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-9">
+                        <div className="row">
+
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <h2>teachers cart</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TeachersContainer;
